7558e6f1d0169822e5c3c5507b0fa7c95178ad8c
Saqib <saqib.bhat.x@gmail.com>
2026-06-28 16:49:10 +0530

    integrate MLflow tracking and enhance production-ready ML pipeline

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,8 +1,14 @@
 import os
 import sys
 import joblib
-
+import mlflow
+import mlflow.sklearn
+sys.path.append(os.path.abspath("."))
 from sklearn.ensemble import RandomForestClassifier
+from src.utils.config import (
+    MODEL_PATH,
+    PREPROCESSOR_PATH,
+    RANDOM_FOREST_CONFIG)
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
@@ -18,6 +24,9 @@ sys.path.append(os.path.abspath("."))
 
 from src.data.preprocess import preprocess_data
 
+mlflow.set_experiment(
+    "Customer Churn Prediction"
+)
 
 def train_model():
 
@@ -28,53 +37,76 @@ def train_model():
         y_test,
         preprocessor
     ) = preprocess_data()
+    with mlflow.start_run():
+        model = RandomForestClassifier(
+            **RANDOM_FOREST_CONFIG
+        )
 
-    model = RandomForestClassifier(
-        n_estimators=300,
-        max_depth=12,
-        min_samples_split=5,
-        min_samples_leaf=2,
-        random_state=42,
-        n_jobs=-1
-    )
+        model.fit(X_train, y_train)
+        mlflow.log_params(
+            RANDOM_FOREST_CONFIG
+            )
+        y_pred = model.predict(X_test)
+        y_prob = model.predict_proba(X_test)[:, 1]
 
-    model.fit(X_train, y_train)
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        roc_auc = roc_auc_score(y_test, y_prob)
+        mlflow.log_metrics({
 
-    y_pred = model.predict(X_test)
-    y_prob = model.predict_proba(X_test)[:, 1]
+            "accuracy": accuracy,
 
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-    roc_auc = roc_auc_score(y_test, y_prob)
+            "precision": precision,
 
-    print("\n" + "=" * 50)
-    print("MODEL PERFORMANCE")
-    print("=" * 50)
+            "recall": recall,
 
-    print(f"Accuracy  : {accuracy:.4f}")
-    print(f"Precision : {precision:.4f}")
-    print(f"Recall    : {recall:.4f}")
-    print(f"F1 Score  : {f1:.4f}")
-    print(f"ROC AUC   : {roc_auc:.4f}")
+            "f1_score": f1,
 
-    print("\nClassification Report")
-    print(classification_report(y_test, y_pred))
+            "roc_auc": roc_auc
 
-    print("\nConfusion Matrix")
-    print(confusion_matrix(y_test, y_pred))
+        })
 
-    os.makedirs("artifacts", exist_ok=True)
+        print("\n" + "=" * 50)
+        print("MODEL PERFORMANCE")
+        print("=" * 50)
 
-    joblib.dump(
-        model,
-        "artifacts/model.pkl"
-    )
+        print(f"Accuracy  : {accuracy:.4f}")
+        print(f"Precision : {precision:.4f}")
+        print(f"Recall    : {recall:.4f}")
+        print(f"F1 Score  : {f1:.4f}")
+        print(f"ROC AUC   : {roc_auc:.4f}")
 
-    print("\nModel saved successfully.")
-    print("Location: artifacts/model.pkl")
+        print("\nClassification Report")
+        print(classification_report(y_test, y_pred))
 
+        print("\nConfusion Matrix")
+        print(confusion_matrix(y_test, y_pred))
+        mlflow.sklearn.log_model(sk_model=model,name="random_forest_model")
+
+        mlflow.log_artifact(
+        MODEL_PATH
+        )
+
+        mlflow.log_artifact(
+        PREPROCESSOR_PATH
+         )
+        os.makedirs("artifacts", exist_ok=True)
+
+        joblib.dump(
+            model,
+            MODEL_PATH
+        )
+
+        joblib.dump(
+            preprocessor,
+            PREPROCESSOR_PATH
+        )
+
+        print("\nArtifacts saved successfully.")
+        print(f"Model Location       : {MODEL_PATH}")
+        print(f"Preprocessor Location: {PREPROCESSOR_PATH}")
 
 if __name__ == "__main__":
     train_model()
--- src/utils/config.py
+++ src/utils/config.py
@@ -7,3 +7,18 @@ MODEL_PATH = os.getenv("MODEL_PATH")
 PREPROCESSOR_PATH = os.getenv("PREPROCESSOR_PATH")
 API_VERSION = os.getenv("API_VERSION")
 PROJECT_NAME = os.getenv("PROJECT_NAME")
+RANDOM_FOREST_CONFIG = {
+
+    "n_estimators": 300,
+
+    "max_depth": 12,
+
+    "min_samples_split": 5,
+
+    "min_samples_leaf": 2,
+
+    "random_state": 42,
+
+    "n_jobs": -1
+
+}
